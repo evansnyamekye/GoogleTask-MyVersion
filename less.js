db29504be159e9fde1992261c98e4cec616a4dbe
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-06-22 09:34:28 +0000

    Add more script functionalities to less.js

--- less.js
+++ less.js
@@ -27,3 +27,29 @@ function arrayDouble (stringArr) {
     console.log(sumResult); 
 }
 arrayDouble(['piggy', 'Bank'])
+
+
+function arraySum (numberArr) {
+    let total = 0; 
+numberArr.forEach(function (num){
+    total += num; 
+}); 
+console.log(total);
+    }
+
+arraySum([1,2,3]); 
+arraySum([5,-2,7,0]); 
+
+function myNum (arrNumber) {
+    let out = 0; 
+    
+    arrNumber.forEach(function (num){
+        out += num; 
+    });
+    console.log(out); 
+}
+
+myNum([2,34,5]); 
+myNum([42,34,25]); 
+myNum([20,314,5]); 
+myNum([-2,-34,55]); 
